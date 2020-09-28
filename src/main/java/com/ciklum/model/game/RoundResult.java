3bc0f4cc80deee72a4035e05af285276f638eedf
Ignacio Gallego Sagastume <igallegosagastume@gmail.com>
2020-09-28 21:05:16 +0200

    Refactor RoundResult to be able to answer which player has won

--- src/main/java/com/ciklum/model/game/RoundResult.java
+++ src/main/java/com/ciklum/model/game/RoundResult.java
@@ -13,13 +13,15 @@ public class RoundResult {
     private final Element e1;
     private final Element e2;
     private final Optional<Player> winner;
-    private final String statsText;
+    private final boolean isPlayer1TheWinner;
+    private final boolean isPlayer2TheWinner;
 
-    public RoundResult(Element e1, Element e2, Optional<Player> winner, String statsText) {
+    public RoundResult(Element e1, Element e2, Optional<Player> winner, boolean isPlayer1TheWinner, boolean isPlayer2TheWinner) {
         this.e1 = e1;
         this.e2 = e2;
         this.winner = winner;
-        this.statsText = statsText;
+        this.isPlayer1TheWinner = isPlayer1TheWinner;
+        this.isPlayer2TheWinner = isPlayer2TheWinner;
     }
 
     /**
@@ -30,6 +32,18 @@ public class RoundResult {
      */
     public Optional<Player> getWinner() {
         return this.winner;
+    }
+
+    public boolean isPlayer1Winner() {
+        return this.isPlayer1TheWinner;
+    }
+
+    public boolean isPlayer2Winner() {
+        return this.isPlayer2TheWinner;
+    }
+
+    public boolean isDraw() {
+        return !this.isPlayer1TheWinner && !this.isPlayer2TheWinner;
     }
 
     public RoundResultVO getVO() {
@@ -43,10 +57,29 @@ public class RoundResult {
                                  winnerString);
     }
 
-    /**
-     * @return the statsText
-     */
-    public String getStatsText() {
-        return statsText;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((e1 == null) ? 0 : e1.hashCode());
+        result = prime * result + ((e2 == null) ? 0 : e2.hashCode());
+        result = prime * result + ( winner.hashCode() );
+        return result;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof RoundResult))
+            return false;
+
+        RoundResult other = (RoundResult) obj;
+
+        return this.e1.equals(other.e1) && this.e2.equals(other.e2) && this.winner.equals(other.winner);
+    }
+
+
 }
